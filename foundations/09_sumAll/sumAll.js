ce5665c7e34e1fc3bcac1966e1fb6f640e3d263d
samyrebs <sam.n.reber@gmail.com>
2026-01-28 14:47:06 -0800

    completed exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(numOne, numTwo) {
+    if((Number.isInteger(numOne) == false) || (Number.isInteger(numTwo) == false)){
+        return "ERROR";
+    }
+    if((numOne < 0) || (numTwo < 0)){
+        return "ERROR";
+    }
+    let bigNum = 0;
+    let smallNum = 0;
+    if(numOne > numTwo){
+        bigNum = numOne;
+        smallNum = numTwo;
+    } else if (numTwo > numOne){
+        bigNum = numTwo;
+        smallNum = numOne;
+    } else {
+        return 0;
+    }
+    let count = 0;
+    for(let i = smallNum; i <= bigNum; i++){
+        count = count + i;
+    }
+    return count;
 };
 
 // Do not edit below this line
