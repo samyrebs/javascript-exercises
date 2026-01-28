f414fb9292a4683679db4de7fe2c9ee89e4a1001
samyrebs <sam.n.reber@gmail.com>
2026-01-28 13:48:54 -0800

    completed exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    strArray.reverse();
+    let newString = strArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
