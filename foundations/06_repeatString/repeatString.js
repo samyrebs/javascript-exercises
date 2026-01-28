72e6d1366d6ffdb18e99704a3c544bc1b0c7ea17
samyrebs <sam.n.reber@gmail.com>
2026-01-28 13:38:22 -0800

    completed exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    let newString = "";
+    if(count < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < count; i++){
+        newString = newString + string;
+    }
+    return newString;
 };
 
 // Do not edit below this line
