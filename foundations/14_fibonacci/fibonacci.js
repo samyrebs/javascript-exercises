a2ae161a1fc0578d0793014d20dffbef4947f775
samyrebs <sam.n.reber@gmail.com>
2026-02-20 20:45:38 -0800

    completed exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0){
+        return "OOPS";
+    }
+    num = Number(num);
+    let num1 = 0;
+    let num2 = 1;
+    for(let i = 1; i <= num; i++){
+        let sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+        console.log(num1+" "+num2);
+    }
+    return num1;
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
