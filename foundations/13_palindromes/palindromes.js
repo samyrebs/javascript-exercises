2a18171f561f512bea99d4b0700c1f7e1178ab83
samyrebs <sam.n.reber@gmail.com>
2026-02-20 20:46:08 -0800

    completed exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789"
+    lowerString = string.toLowerCase();
+    console.log(lowerString);
+    let arr = lowerString.split('');
+    console.log(arr);
+    cleanArray = arr.filter(item => alphanumerical.includes(item));
+    console.log(cleanArray);
+    let cleanString = cleanArray.join('');
+    cleanArray.reverse();
+    console.log(cleanArray);
+    let reverseString = cleanArray.join('');
+    console.log(reverseString);
+    if(reverseString == cleanString){
+        return true;
+    } else {
+        return false;
+    }
 };
-
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
