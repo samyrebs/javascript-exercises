ee3822976957dcac0b7ff6a585df3a840aa1ba17
samyrebs <sam.n.reber@gmail.com>
2026-02-20 19:57:09 -0800

    completed exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((sum, current) => sum * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let temp = 1;
+	for(let i = 1; i <= num; i++){
+    temp = temp * i;
+  }
+  return temp;
 };
 
 // Do not edit below this line
